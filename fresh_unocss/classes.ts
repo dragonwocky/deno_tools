48c96b03a2e3bed08086ff98d679cfb9408ec234
dragonwocky <thedragonring.bod@gmail.com>
2022-06-26 18:21:20 +1000

    fresh_unocss: don't expand (groups) within [computed classes]

--- fresh_unocss/classes.ts
+++ fresh_unocss/classes.ts
@@ -8,28 +8,47 @@ import fnv1a from "https://esm.sh/fnv1a@1.1.1";
 const _shortcuts: [string, string][] = [],
   compiledShortcuts = () => _shortcuts;
 
-const variantGroupPattern = /([^\s\('"`;>=\\]+)\((([^(]|(?<=\\)\()*?[^\\])\)/g,
-  variantGroupReplacer = (_: string, variant: string, group: string) =>
-    group.split(/(?<!\[[^\]]*?)\s+(?![^\[]*?\])/)
-      .map((utility: string) => variant + utility).join(" "),
-  parseTaggedTemplate = (
+type AcceptsStringOrTemplateString = (
+  cls: string | TemplateStringsArray,
+  ...tmpls: unknown[]
+) => string;
+const parseTaggedTemplate = (
     strs: TemplateStringsArray,
     ...tmpls: unknown[]
   ): string => strs.map((str, i) => str + (tmpls[i] ?? "")).join(""),
   hashFromString = (str: string) =>
     fnv1a(str, 32).toString(16).padStart(2, "0").slice(0, 8);
 
-type AcceptsStringOrTemplateString = (
-  cls: string | TemplateStringsArray,
-  ...tmpls: unknown[]
-) => string;
+const roundBracketsWithinSquareBracketsPattern =
+    /\[[^\]]*?(?<!\\)(\(|\))(?=[^\]]*?])/g,
+  variantGroupPattern = /([^\s\('"`;>=\\]+)\((([^(]|(?<=\\)\()*?[^\\])\)/g,
+  escapedRoundBracketsPattern = /\\(\(|\))/g,
+  roundBracketsWithinSquareBracketsReplacer = (match: string) =>
+    match.replaceAll(/(?<!\\)(\(|\))/g, `\\$&`),
+  variantGroupReplacer = (_: string, variant: string, group: string) =>
+    group.split(/(?<!\[[^\]]*?)\s+(?![^\[]*?\])/)
+      .map((utility: string) => variant + utility).join(" "),
+  escapedRoundBracketsReplacer = (_: string, bracket: string) => bracket;
+
 const interpret: AcceptsStringOrTemplateString = (cls, ...tmpls): string => {
     // e.g. dark:(font-bold m(x-1 y-2) border-(red-200 1))
     // --> dark:font-bold dark:mx-1 dark:my-2 dark:border-red-200 dark:border-1
     if (typeof cls !== "string") cls = parseTaggedTemplate(cls, tmpls);
     cls = cls.replaceAll(/\s+/g, " ").trim();
+    while (roundBracketsWithinSquareBracketsPattern.test(cls)) {
+      cls = cls.replaceAll(
+        roundBracketsWithinSquareBracketsPattern,
+        roundBracketsWithinSquareBracketsReplacer,
+      );
+    }
     while (variantGroupPattern.test(cls)) {
       cls = cls.replaceAll(variantGroupPattern, variantGroupReplacer);
+    }
+    while (escapedRoundBracketsPattern.test(cls)) {
+      cls = cls.replaceAll(
+        escapedRoundBracketsPattern,
+        escapedRoundBracketsReplacer,
+      );
     }
     return cls;
   },
