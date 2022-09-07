0e817e67a1aa4236d05ff2dde2c9c188b3a414cf
dragonwocky <thedragonring.bod@gmail.com>
2022-09-07 20:59:50 +1000

    fresh_unocss: update deps + inc. code elegance & perf

--- fresh_unocss/runtime.ts
+++ fresh_unocss/runtime.ts
@@ -2,77 +2,72 @@
 
 import fnv1a from "https://esm.sh/fnv1a@1.1.1";
 
-// separated from server.ts so classes can be compiled within
-// islands without sending server-side code to the client
-
-const compiledClassAliases: [string, string][] = [],
-  getCompiledClassAliases = () => compiledClassAliases;
-
-type AcceptsStringOrTemplateString = (
+// parses a tagged template into an ordinary string
+// e.g. so that f('string') acts identically to f`string`
+type TemplateableString = (
   cls: string | TemplateStringsArray,
   ...tmpls: unknown[]
 ) => string;
 const parseTaggedTemplate = (
-    strs: TemplateStringsArray,
-    ...tmpls: unknown[]
-  ): string => strs.map((str, i) => str + (tmpls[i] ?? "")).join(""),
-  hashFromString = (str: string) =>
-    fnv1a(str, 32).toString(16).padStart(2, "0").slice(0, 8);
+  strs: TemplateStringsArray,
+  ...tmpls: unknown[]
+): string => strs.map((str, i) => str + (tmpls[i] ?? "")).join("");
 
-const roundBracketsWithinArbitraryValuesPattern =
-    /\[[^\]]*?(?<!\\)(\(|\))(?=[^\]]*?])/g,
-  variantGroupPattern = /([^\s\('"`;>=\\]+)\((([^(]|(?<=\\)\()*?[^\\])\)/g,
-  escapedRoundBracketsPattern = /\\(\(|\))/g,
-  roundBracketsWithinArbitraryValuesReplacer = (match: string) =>
-    match.replaceAll(/(?<!\\)(\(|\))/g, `\\$&`),
-  variantGroupReplacer = (_: string, variant: string, group: string) =>
-    group.split(/(?<!\[[^\]]*?)\s+(?![^\[]*?\])/)
-      .map((utility: string) => variant + utility).join(" "),
-  escapedRoundBracketsReplacer = (_: string, bracket: string) => bracket;
+// fnv1a: a non-cryptographic hash function, generating hashes
+// fast with a low-collision rate. used for unique alias
+// generation for compiled classes
+const hashFromString = (str: string) =>
+    fnv1a(str, 32).toString(16).padStart(2, "0").slice(0, 8),
+  compiledClassAliases: [string, string][] = [],
+  addCompiledClassAlias = (alias: string, cls: string) => {
+    const alreadyExists = compiledClassAliases.some(([a]) => a === alias);
+    if (!alreadyExists) compiledClassAliases.push([alias, cls]);
+    return alias;
+  },
+  _getCompiledClassAliases = () => compiledClassAliases;
 
-type Replacer = Parameters<typeof String.prototype.replaceAll>[1];
-const replaceAll = (
-  str: string,
-  pattern: RegExp,
-  replacer: string | Replacer,
-) => {
-  while (pattern.test(str)) str = str.replaceAll(pattern, replacer as Replacer);
-  return str;
-};
+// replacers used to expand variant groups safely
+const escapeArbitraryValues = (cls: string): string => {
+    // css funcs used in arbitrary values should not be expanded
+    // e.g. h-[calc(100%-16px)]
+    const pattern = /\[[^\]]*?(?<!\\)(\(|\))(?=[^\]]*?])/g;
+    cls = cls.replaceAll(pattern, (match) => {
+      return match.replaceAll(/(?<!\\)(\(|\))/g, `\\$&`);
+    });
+    return pattern.test(cls) ? escapeArbitraryValues(cls) : cls;
+  },
+  expandVariantGroups = (cls: string): string => {
+    const pattern = /([^\s\('"`;>=\\]+)\((([^(]|(?<=\\)\()*?[^\\])\)/g,
+      splitter = /(?<!\[[^\]]*?)\s+(?![^\[]*?\])/;
+    cls = cls.replaceAll(pattern, (_, variant, group) => {
+      return group.split(splitter)
+        .map((utility: string) => variant + utility).join(" ");
+    });
+    return pattern.test(cls) ? expandVariantGroups(cls) : cls;
+  },
+  unescapeRoundedBrackets = (cls: string): string => {
+    const pattern = /\\(\(|\))/g;
+    cls = cls.replaceAll(pattern, (_, bracket) => bracket);
+    return pattern.test(cls) ? unescapeRoundedBrackets(cls) : cls;
+  };
 
-const interpret: AcceptsStringOrTemplateString = (cls, ...tmpls): string => {
+// classname transformers to extend the useability of unocss
+const interpret: TemplateableString = (cls, ...tmpls): string => {
     // e.g. dark:(font-bold m(x-1 y-2) border-(red-200 1))
     // --> dark:font-bold dark:mx-1 dark:my-2 dark:border-red-200 dark:border-1
     if (typeof cls !== "string") cls = parseTaggedTemplate(cls, tmpls);
-    cls = replaceAll(cls, /\s\s+/g, " ").trim();
-    // css funcs used in arbitrary values should not be expanded
-    // e.g. h-[calc(100%-16px)]
-    cls = replaceAll(
-      cls,
-      roundBracketsWithinArbitraryValuesPattern,
-      roundBracketsWithinArbitraryValuesReplacer,
-    );
-    cls = replaceAll(
-      cls,
-      variantGroupPattern,
-      variantGroupReplacer,
-    );
-    cls = replaceAll(
-      cls,
-      escapedRoundBracketsPattern,
-      escapedRoundBracketsReplacer,
-    );
+    cls = cls.replaceAll(/\s+/g, " ").trim();
+    cls = escapeArbitraryValues(cls);
+    cls = expandVariantGroups(cls);
+    cls = unescapeRoundedBrackets(cls);
     return cls;
   },
-  compile: AcceptsStringOrTemplateString = (cls, ...tmpls): string => {
+  compile: TemplateableString = (cls, ...tmpls): string => {
     // e.g. dark:(font-bold m(x-1 y-2) border-(red-200 1))
     // --> uno-68ff7987
     cls = interpret(cls, ...tmpls);
     const hashed = `uno-${hashFromString(cls)}`;
-    if (!compiledClassAliases.find(([h]) => h === hashed)) {
-      compiledClassAliases.push([hashed, cls]);
-    }
-    return hashed;
+    return addCompiledClassAlias(hashed, cls);
   };
 
-export { compile, getCompiledClassAliases, interpret };
+export { _getCompiledClassAliases, compile, interpret };
--- fresh_unocss/server.ts
+++ fresh_unocss/server.ts
@@ -1,124 +1,123 @@
 // (c) dragonwocky <thedragonring.bod@gmail.com> (https://dragonwocky.me/)
 
-import { RenderFunction } from "https://deno.land/x/fresh@1.0.1/server.ts";
-import { getCompiledClassAliases } from "./runtime.ts";
-
+import { type RenderFunction } from "$fresh/server.ts";
 import {
   default as initParcel,
   transform,
-} from "https://esm.sh/@parcel/css-wasm@1.10.1";
+} from "https://esm.sh/@parcel/css-wasm@1.13.1";
 import {
   createGenerator,
+  type UnoConfig,
   type UnoGenerator,
-  type UserConfig,
-} from "https://esm.sh/@unocss/core@0.43.0";
-import presetUno from "https://esm.sh/@unocss/preset-uno@0.43.0";
-import presetIcons from "https://esm.sh/@unocss/preset-icons@0.43.0?no-require";
-import presetWebFonts from "https://esm.sh/@unocss/preset-web-fonts@0.43.0";
-import presetTypography from "https://esm.sh/@unocss/preset-typography@0.43.0";
-
-const wasmInitUrl =
-  "https://cdn.esm.sh/@parcel/css-wasm@1.10.1/parcel_css_node_bg.wasm";
-// @ts-ignore: provide absolute path to wasm module
-await initParcel(new URL(wasmInitUrl));
+} from "https://esm.sh/@unocss/core@0.45.18";
+import presetIcons from "https://esm.sh/@unocss/preset-icons@0.45.18?no-require";
+import presetTypography from "https://esm.sh/@unocss/preset-typography@0.45.18";
+import presetUno from "https://esm.sh/@unocss/preset-uno@0.45.18";
+import presetWebFonts from "https://esm.sh/@unocss/preset-web-fonts@0.45.18";
+import { _getCompiledClassAliases } from "./runtime.ts";
 
 // @parcel/css provides better minification than unocss' builtin
 // { minify: true } output option and is necessary to properly
 // minify preset-generated css e.g. preflights, @font-face rules
+let useParcel: boolean;
+const wasmInitUrl =
+    "https://cdn.esm.sh/@parcel/css-wasm@1.13.1/parcel_css_node_bg.wasm",
+  minify = (css: string | Uint8Array, filename = "styles.css") => {
+    const { code } = transform({
+      filename,
+      code: css instanceof Uint8Array ? css : new TextEncoder().encode(css),
+      minify: true,
+    });
+    return new TextDecoder().decode(code);
+  };
 
-const minify = (css: string | Uint8Array, filename = "styles.css") => {
-  const { code } = transform({
-    filename,
-    code: css instanceof Uint8Array ? css : new TextEncoder().encode(css),
-    minify: true,
-  });
-  return new TextDecoder().decode(code);
-};
-
+// modern-normalize: normalises and resets default browser styling
 const cssResetUrl = "https://esm.sh/modern-normalize@1.1.0?css",
   cssReset = await fetch(cssResetUrl).then((res) => res.text());
 
-// unocss: generates on-demand atomic css from strings of
-// html on the server-side. imports shortcuts from
-// client.ts to support compiled classes
-
-let uno: ReturnType<typeof createGenerator> | undefined;
-
-const setup = (
-  config: UserConfig = {},
-  extendDefaults = true,
-): UnoGenerator => {
-  return uno = createGenerator({
-    ...config,
-    presets: extendDefaults
-      ? [
-        presetUno({ dark: "class", variablePrefix: "uno-" }),
-        presetIcons({
-          cdn: "https://esm.sh/",
-          extraProperties: { "display": "inline-block" },
-        }),
-        presetTypography({
-          cssExtend: {
-            // make code blocks look nice
-            "pre": {
-              "max-width": "100%",
-              "min-width": "100%",
-              "display": "inline-block",
-              "padding": "1.25rem 0",
-            },
-            "code:not(pre code)": {
-              "padding": "0 0.3em",
-              "margin": "0 0.1em",
-            },
-            "pre, code": {
-              "color": "var(--prism-fg, var(--un-prose-code))",
-              "background": "var(--prism-bg, transparent)",
-            },
-            "pre, code:not(pre code)": {
-              "border": "1px solid var(--un-prose-borders)",
-              "border-radius": "0.375rem",
-            },
-            // disable unnecessary margins at the bottom of <details> elems
-            "details > *:last-child": {
-              "margin-bottom": "0",
-            },
-          },
-        }),
-        presetWebFonts({
-          fonts: {
-            sans: "Inter",
-            mono: "Fira Code",
-          },
-        }),
-        ...(config.presets ?? []),
-      ]
-      : config.presets,
-    preflights: extendDefaults
-      ? [{ getCSS: () => cssReset }, ...(config.preflights ?? [])]
-      : config.preflights,
-  });
-};
+// unocss: generates on-demand atomic css from strings of html
+let uno: UnoGenerator;
+const defaultPresets = [
+  presetUno({ dark: "class", variablePrefix: "uno-" }),
+  presetIcons({
+    cdn: "https://esm.sh/",
+    extraProperties: { "display": "inline-block" },
+  }),
+  presetTypography({
+    cssExtend: {
+      // make code blocks look nice
+      "pre": {
+        "max-width": "100%",
+        "min-width": "100%",
+        "display": "inline-block",
+        "padding": "1.25rem 0",
+      },
+      "code:not(pre code)": {
+        "padding": "0 0.3em",
+        "margin": "0 0.1em",
+      },
+      "pre, code": {
+        "color": "var(--prism-fg, var(--un-prose-code))",
+        "background": "var(--prism-bg, transparent)",
+      },
+      "pre, code:not(pre code)": {
+        "border": "1px solid var(--un-prose-borders)",
+        "border-radius": "0.375rem",
+      },
+      // disable unnecessary margins at the bottom of <details> elems
+      "details > *:last-child": {
+        "margin-bottom": "0",
+      },
+    },
+  }),
+  presetWebFonts({
+    fonts: {
+      sans: "Inter",
+      mono: "Fira Code",
+    },
+  }),
+];
 
 const render: RenderFunction = async (ctx, render) => {
-  if (!uno) throw new Error("fresh_unocss: setup() must be called!");
-
-  // get styles from prev. render in same ctx
-  let [html = "", styles = ""] = (ctx.state.get("uno") ?? []) as string[];
-  // remove prev styles
-  const prevStylesIndex = ctx.styles.findIndex((s) => s === styles);
+  if (!uno) throw new Error("fresh_unocss: await setup() must be called!");
+  // extract styles from prev. render in same ctx
+  let [htmlText = "", cssText = ""] = (ctx.state.get("uno") ?? []) as string[];
+  const prevStylesIndex = ctx.styles.findIndex((css) => css === cssText);
   if (prevStylesIndex > -1) ctx.styles.splice(prevStylesIndex, 1);
-
   // render page and components
-  html += render();
-  // inform uno of compiled class aliases (must be done post-render)
-  uno.config.shortcuts.push(...getCompiledClassAliases());
+  htmlText += render();
+  // inform uno of compiled class aliases (must run post-render)
+  uno.config.shortcuts.push(..._getCompiledClassAliases());
   uno.config.shortcuts = [...new Set(uno.config.shortcuts)];
   // generate new css
-  styles = minify((await uno.generate(html)).css);
-  ctx.styles.unshift(styles);
-
+  cssText = (await uno.generate(htmlText)).css;
+  if (useParcel) cssText = minify(cssText);
   // persist for next render in same ctx
-  ctx.state.set("uno", [html, styles]);
+  ctx.styles.unshift(cssText);
+  ctx.state.set("uno", [htmlText, cssText]);
 };
 
-export { minify, render, setup };
+type PluginConfig = {
+  extendDefaults?: boolean;
+  useParcel?: boolean;
+};
+const setup = async (config: UnoConfig & PluginConfig = {}) => {
+  config.extendDefaults ??= true;
+  config.presets ??= [];
+  config.preflights ??= [];
+  uno = createGenerator({
+    ...config,
+    presets: config.extendDefaults
+      ? [...defaultPresets, ...config.presets]
+      : config.presets,
+    preflights: config.extendDefaults
+      ? [{ getCSS: () => cssReset }, ...config.preflights]
+      : config.preflights,
+  });
+  config.useParcel ??= true;
+  // @ts-ignore: provide absolute path to wasm module
+  if (config.useParcel) await initParcel(new URL(wasmInitUrl));
+  useParcel = config.useParcel;
+};
+
+export { render, setup };
